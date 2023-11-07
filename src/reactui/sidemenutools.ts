1d16f7f432bd7e53969c98dc254904a5dedd4d7d
Aelonnnn <147461798+Aelonnnn@users.noreply.github.com>
2023-11-07 16:01:30 -0500

    Update sidemenutools.ts

--- src/reactui/sidemenutools.ts
+++ src/reactui/sidemenutools.ts
@@ -5,24 +5,6 @@
 module CityGame
 {
   {
-  export module Tools
-  {
-    export class RoadTool extends Tool
-    {
-      constructor()
-      {
-        super();
-        this.type = "road";
-        this.selectType = SelectionTypes.manhattanSelect;
-        this.tintColor = 0x696969;
-      } 
-      onActivate(target: Cell)
-      {
-        target.changeContent( cg["content"]["roads"]["road_nesw"] );
-      }
-    }
-  }
-}
   export module UIComponents
   {
   
@@ -128,6 +110,24 @@ module CityGame
       {
         props[selectedTool].className += " selected-tool";
       }
+       export module Tools
+  {
+    export class RoadTool extends Tool
+    {
+      constructor()
+      {
+        super();
+        this.type = "road";
+        this.selectType = SelectionTypes.manhattanSelect;
+        this.tintColor = 0x696969;
+      } 
+      onActivate(target: Cell)
+      {
+        target.changeContent( cg["content"]["roads"]["road_nesw"] );
+      }
+    }
+  }
+}
   
   
       return(
